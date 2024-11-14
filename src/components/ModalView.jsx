f2f1dbb903f4352897ce3692d5f16aa9f1511405
Qasim <mq12113@gmail.com>
2024-11-14 22:17:02 +0000

    updated modal

--- src/components/ModalView.jsx
+++ src/components/ModalView.jsx
@@ -1,5 +1,7 @@
-import { View } from "@react-three/drei";
-import React from "react";
+import { PerspectiveCamera, View } from "@react-three/drei";
+import React, { Suspense } from "react";
+import Lights from "./Lights";
+import IPhone from "./IPhone";
 
 const ModalView = ({
   index,
@@ -11,8 +13,22 @@ const ModalView = ({
   size,
   item,
 }) => {
-  return (<View index={index}>
+  return (
+    
+  <View index={index}
+  id={gsapType}
+  className={`border-2 border-red-500 w-full h-full
+  {index === 2} ? right-[100%] : ''`}
+  >
   
+  { /*Ambient light */}
+  <ambientLight intensity={0.3} />
+  <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+  <Lights />
+  <Suspense fallback={<div>Loading...</div>}> 
+  <IPhone />
+  </Suspense>
+
   </View>) }
 
 export default ModalView;
